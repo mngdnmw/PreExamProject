61d74c62a24e1572e7eeff843252f26dc03ea065
mngdnmw <mtdunmow@gmail.com>
2017-10-23 15:05:01 +0200

    First commit

---
+++ DemoBLL/IService.cs
@@ -0,0 +1,19 @@
+﻿using DemoBLL.BusinessObjects;
+using System.Collections.Generic;
+
+namespace DemoBLL
+{
+    public interface IService<IBusinessObject>
+    {
+        //C
+        IBusinessObject Create(IBusinessObject bo);
+        //R
+        List<IBusinessObject> GetAll();
+        IBusinessObject Get(int Id);
+        //U
+        IBusinessObject Update(IBusinessObject bo);
+        //D
+        IBusinessObject Delete(int Id);
+    }
+    
+}
